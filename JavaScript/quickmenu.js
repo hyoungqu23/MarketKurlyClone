77acfb57c019628a190b4ffb442cf983f247a2ba
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-05-08 22:20:31 +0900

    0508 quick-menu scroll sticky transition

--- JavaScript/quickmenu.js
+++ JavaScript/quickmenu.js
@@ -4,3 +4,8 @@ window.onresize = () => {
   let x = window.innerWidth;
   x < 1250 ? quickMenu.style.display = "none" : quickMenu.style.display = "block";
 }
+
+window.addEventListener("scroll", () => {
+  let y = +window.scrollY;
+  (y > 720) ? quickMenu.style.top = y + 320 + "px" : quickMenu.style.top = "730px"
+})
